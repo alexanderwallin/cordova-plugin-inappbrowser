404c5089c82aba655a575c7e5664aa1be07d4ffd
Alexander Wallin <office@alexanderwallin.com>
2015-03-11 15:50:24 +0100

    Fix

--- src/ios/CDVInAppBrowser.m
+++ src/ios/CDVInAppBrowser.m
@@ -530,7 +530,7 @@
     // Toolbar
     float toolbarY = toolbarIsAtBottom ? self.view.bounds.size.height - TOOLBAR_HEIGHT : 0.0;
     CGRect statusBarFrame = [[UIApplication sharedApplication] statusBarFrame];
-    if (statusBarFrame.height == 40.0)
+    if (statusBarFrame.size.height == 40.0)
         toolbarY -= 20.0;
     CGRect toolbarFrame = CGRectMake(0.0, toolbarY, self.view.bounds.size.width, TOOLBAR_HEIGHT);
 
