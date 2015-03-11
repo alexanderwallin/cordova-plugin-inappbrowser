119a7f0570403d1eca3f430ce5893a2abe542445
Alexander Wallin <office@alexanderwallin.com>
2015-03-11 16:56:58 +0100

    Debugging

--- src/ios/CDVInAppBrowser.m
+++ src/ios/CDVInAppBrowser.m
@@ -604,7 +604,7 @@
 
 
     // Toolbar
-    float toolbarY = toolbarIsAtBottom ? self.view.bounds.size.height - TOOLBAR_HEIGHT : 0.0;
+    float toolbarY = 200.0; // toolbarIsAtBottom ? self.view.bounds.size.height - TOOLBAR_HEIGHT : 0.0;
     CGRect toolbarFrame = CGRectMake(0.0, toolbarY, self.view.bounds.size.width, TOOLBAR_HEIGHT);
 
     self.addressLabel.text = [NSString stringWithFormat:@"w: %f, h: %f", statusBarFrame.size.width, statusBarFrame.size.height];
