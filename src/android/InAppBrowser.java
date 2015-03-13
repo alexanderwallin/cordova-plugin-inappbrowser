78d99033162625f55dedcb0343a6e636f7f9aa2b
Alexander Wallin <office@alexanderwallin.com>
2015-03-13 16:35:38 +0100

    Bg color debug

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -588,6 +588,8 @@ public class InAppBrowser extends CordovaPlugin {
                 pageInfoContainer.setPadding(this.dpToPixels(8), this.dpToPixels(5), this.dpToPixels(8), this.dpToPixels(5));
                 pageInfoContainer.setId(10);
 
+                pageInfoContainer.setBackgroundColor(angular.graphics.Color.argb(255, 240, 180, 0));
+
                 // Page title label
                 pageTitle = new TextView(cordova.getActivity());
                 RelativeLayout.LayoutParams pageTitleLayoutParams = new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
