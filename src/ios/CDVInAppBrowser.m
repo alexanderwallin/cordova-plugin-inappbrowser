948fc07117ac979da6676795d7080eab2d0cd387
Alexander Wallin <office@alexanderwallin.com>
2015-03-18 17:30:40 +0100

    20px top

--- src/ios/CDVInAppBrowser.m
+++ src/ios/CDVInAppBrowser.m
@@ -638,7 +638,7 @@
     self.pageTitle = pageInfoView; // [[UIBarButtonItem alloc] initWithCustomView:pageInfoView];
 
     // Toolbar
-    float toolbarY = toolbarIsAtBottom ? self.view.bounds.size.height - TOOLBAR_HEIGHT : 0.0;
+    float toolbarY = toolbarIsAtBottom ? self.view.bounds.size.height - TOOLBAR_HEIGHT : 20.0;
     CGRect toolbarFrame = CGRectMake(0.0, toolbarY, self.view.bounds.size.width, TOOLBAR_HEIGHT);
 
     self.toolbarPlain = [[UIView alloc] initWithFrame:toolbarFrame];
