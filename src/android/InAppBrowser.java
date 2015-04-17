60e942fa6592d09d0ae9bf6d920fd78aadb268a3
Alexander Wallin <office@alexanderwallin.com>
2015-04-17 21:11:47 +0200

    Android: change x padding, not y

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -615,7 +615,7 @@ public class InAppBrowser extends CordovaPlugin {
                 pageInfoContainer.setLayoutParams(pageInfoLayoutParams);
                 pageInfoContainer.setHorizontalGravity(Gravity.LEFT);
                 pageInfoContainer.setVerticalGravity(Gravity.CENTER_VERTICAL);
-                pageInfoContainer.setPadding(this.dpToPixels(8), this.dpToPixels(2), this.dpToPixels(8), this.dpToPixels(2));
+                pageInfoContainer.setPadding(this.dpToPixels(2), this.dpToPixels(7), this.dpToPixels(2), this.dpToPixels(7));
                 pageInfoContainer.setId(10);
 
                 // Page title label
